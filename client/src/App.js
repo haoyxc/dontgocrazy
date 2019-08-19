2341e3d76ddf5f4774eb1b47127c11eac99a0a47
bsitenga <sitenga@usc.edu>
2019-08-19 16:33:19 -0400

    Completed routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,33 @@
 import React, {useState} from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import AllTimes from './components/AllTimes';
+import Insights from './components/Insights';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChartBar, faHistory, faChartLine, faPaw } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   const id = localStorage.getItem("userId");
+  const [currentPage, setCurrentPage] = useState(<Dashboard />)
 	return (
 		<div className="container-main">
 			<header className="container-header">
-				<button className="logo">
+				<button className="logo" onClick = {() => setCurrentPage(<Dashboard />)}>
 					<FontAwesomeIcon icon={faPaw} />
 				</button>
-				<button className="active">
+				<button className="active" onClick = {() => setCurrentPage(<Dashboard />)}>
 					<FontAwesomeIcon icon={faChartBar} /> Dashboard
 				</button>
 				<div />
-				<button>
+				<button onClick = {() => setCurrentPage(<AllTimes />)}>
 					<FontAwesomeIcon icon={faHistory} /> All Times
 				</button>
 				<div />
-				<button>
+				<button onClick = {() => setCurrentPage(<Insights />)}>
 					<FontAwesomeIcon icon={faChartLine} /> Insights
 				</button>
 			</header>
-			<currentPage />
+			{currentPage}
 		</div>
 	);
 }
