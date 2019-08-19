07e298e6fa13af72a227f81a6235d842999abf66
bsitenga <sitenga@usc.edu>
2019-08-19 11:44:49 -0400

    created doughnut chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,44 @@
-import React from "react";
-import "./App.css";
-import PieChart from "react-minimal-pie-chart";
+import React from 'react';
+import './App.css';
+import { Doughnut } from 'react-chartjs-2';
 
 function App() {
-  return (
-    <div className="container-main">
-      <header className="container-header">Your stats for today:</header>
-      <div className="piechart-container">
-        <PieChart
-          data={[
-            { title: "One", value: 10, color: "#E38627" },
-            { title: "Two", value: 15, color: "#C13C37" },
-            { title: "Three", value: 20, color: "#6A2135" }
-          ]}
-        />
-      </div>
-      <div>Main body content</div>
-    </div>
-  );
+	return (
+		<div className="container-main">
+			<header className="container-header">
+        Navbar
+      </header>
+			<div className="piechart-container">
+				<Doughnut
+					data={{
+						datasets: [
+							{
+                data: [ 30, 20, 10, 30, 20, 10, 30, 20, 10 ].sort((a, b) => b - a),
+                backgroundColor: [
+                  '#ff6363',
+                  '#ffa463',
+                  '#fff763',
+                  '#9fff63',
+                  '#63ff75',
+                  '#63ffea',
+                  '#63a1ff',
+                  '#7a63ff',
+                  '#a763ff'
+                ]
+							}
+						],
+            labels: [ 'Facebook', 'Leetcode', 'Google', 'Leetcode','Youtube','Cooldox','Instagram','Mealpal','Amazon'],
+          }}
+          options={{
+            legend: {
+              display: false
+            }
+          }}
+				/>
+			</div>
+			{/* <div>Main body content</div> */}
+		</div>
+	);
 }
 
 export default App;
