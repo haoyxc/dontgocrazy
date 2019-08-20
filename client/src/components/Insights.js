d62e069cd208cf1102c5256b6523b1a836f29bc4
uchenma <uchenm@seas.upenn.edu>
2019-08-20 13:47:13 -0400

    initial insight page set up

--- client/src/components/Insights.js
+++ client/src/components/Insights.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Insights() {
+	const [ day, setDay ] = useState(new Date());
+
+	useEffect(()=> {
+		fetch('https://tranquil-wildwood-15780.herokuapp.com/allStats/' + localStorage.getItem('userId'))
+			.then(function(response) {
+				return response.json();
+			})
+			.then(function(myJson) {
+				return myJson.stats;
+			})
+			.then(function (stats) {
+				console.log(stats)
+			})
+	},[])
+
 	return (
 			<div className="piechart-container">
-				Insights
+				Insight 
 			</div>
 	);
 }
